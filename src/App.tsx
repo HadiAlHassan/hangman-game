a4d5b22286415815631afe19424ea3bb5a7fb938
Hadi Al Hassan <hadi.alhassan@wafeq.com>
2026-01-12 00:27:27 +0200

    brain storming rough structure of game

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import './App.css'
+import React from 'react';
+import './styles-root/styles.scss';
 
 function App() {
-
+  const [currentPlayer, setCurrentPlayer] = React.useState(1);
   return (
     <>
+    <div className='playerindiccont'>
+      <div  className='indicator'>Player {currentPlayer}'s turn</div>
+    </div>
+    <button onClick={()=>setCurrentPlayer(3-currentPlayer)}> Change Player</button>
+    
+    <div className='play-area'>
+     {currentPlayer == 1? 
+     (
+      <>
+      <div className='player-1'>
+        <label>Enter Word </label>
+        <input></input>
+        <button onClick={()=>{}}>Submit</button>
+      </div>
+     </>):
+     (<>
+     <div className='player-2'>
+      <div className='guess-count-display'></div>
+      <label>Enter 1 Letter </label>
+      <input></input>
+      <button>Submit</button>
+      </div></>)}
+    </div>
     </>
   )
 }
---
+++ src/components/currentPlayerIndicator/currentPlayerIndicator.tsx
@@ -0,0 +1,8 @@
+
+export default function CurrentPlayerIndicator(player:string){
+    return (
+    <div>
+        {player}
+    </div>
+    );
+}
